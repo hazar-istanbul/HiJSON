5f58d746c6db1397fd8d8cf2cc152edeca0c8836
Hazar Istanbul <47646544+hazar-istanbul@users.noreply.github.com>
2019-05-24 18:09:24 +0300

    Update add.php

--- add.php
+++ add.php
@@ -1,7 +1,7 @@
-if($_POST){
-    $title = $_POST['title'];
-    $data = file_get_contents('data.json');
-    $json_arr = json_decode($data, true);
-    $json_arr[] = array('title'=>''.$title.'');
-    file_put_contents('data.json', json_encode($json_arr));
-}
+<?php
+$title = $_POST['title'];
+$data = file_get_contents('data.json');
+$json_arr = json_decode($data, true);
+$json_arr[] = array('title'=>''.$title.'');
+file_put_contents('data.json', json_encode($json_arr));
+?>
